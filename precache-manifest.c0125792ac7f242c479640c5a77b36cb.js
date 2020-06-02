50f2aa77769c03adf0d705b78847c5c13be6c568
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-06-02 19:09:13 +0300

    Updates

---
+++ precache-manifest.c0125792ac7f242c479640c5a77b36cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dc808d3d148f7c3fc06ac6e365be5a",
+    "url": "/messenger-app/index.html"
+  },
+  {
+    "revision": "571d9a7282411a72022e",
+    "url": "/messenger-app/static/css/main.ab9658ff.chunk.css"
+  },
+  {
+    "revision": "b0bc82266ef587cc18a8",
+    "url": "/messenger-app/static/js/2.7fe93077.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/messenger-app/static/js/2.7fe93077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571d9a7282411a72022e",
+    "url": "/messenger-app/static/js/main.dbc182e8.chunk.js"
+  },
+  {
+    "revision": "e65e0ad083b444798f34",
+    "url": "/messenger-app/static/js/runtime-main.8b0a021a.js"
+  },
+  {
+    "revision": "0016bc58cbbce11a7b5ed8fd3d1a2bcf",
+    "url": "/messenger-app/static/media/alice.0016bc58.webp"
+  },
+  {
+    "revision": "30f622fa015c04e0ea9293f09068fed4",
+    "url": "/messenger-app/static/media/bareira.30f622fa.webp"
+  },
+  {
+    "revision": "5098214dd7c76f134fee1217da1ac58a",
+    "url": "/messenger-app/static/media/checked.5098214d.svg"
+  },
+  {
+    "revision": "3df665c5d1adab9cb455450994f3ff53",
+    "url": "/messenger-app/static/media/josefiena.3df665c5.webp"
+  },
+  {
+    "revision": "20bdba7b3fc3e998ebca059fdec0ccb0",
+    "url": "/messenger-app/static/media/me.20bdba7b.webp"
+  },
+  {
+    "revision": "2150318fbf59e9da97021a74e1c79eaf",
+    "url": "/messenger-app/static/media/searchIcon.2150318f.svg"
+  },
+  {
+    "revision": "f1f16c142c49647287dbb055a66e2fc3",
+    "url": "/messenger-app/static/media/sendIcon.f1f16c14.svg"
+  },
+  {
+    "revision": "37ff2bac79e6ba5d236f41ba12a597b9",
+    "url": "/messenger-app/static/media/velazzquez.37ff2bac.webp"
+  }
+]);
